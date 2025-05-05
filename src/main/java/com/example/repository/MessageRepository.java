4afd32b6e27bbf0d9036a8d318612e3593c7615c
Robby Martin <rrdmartin80@gmail.com>
2025-05-05 15:29:41 +0000

    Updated deleteMessageById

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.entity.Message;
 
@@ -17,6 +19,6 @@ public interface MessageRepository extends JpaRepository<Message, Integer> {
 
     Optional<Message> findByMessageId(Integer messageId);
 
-    @Query("DELETE FROM Message m WHERE m.messageId = :messageId")
-    Integer deleteByMessageId(@Param("messageId") Integer messageId);
+    @Transactional
+    Integer deleteByMessageId(Integer messageId);
 }
